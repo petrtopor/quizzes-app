bd81846bba24e9e03416b4b39f3e4d220d8f1a47
Vadim Kushir <vadim.kushnir@logity.tech>
2024-03-23 04:37:01 +0500

    feat: implemtnted the quiz tile card component

--- src/components/QuizCard.vue
+++ src/components/QuizCard.vue
@@ -1,25 +1,82 @@
 <template>
   <v-card
-    class="py-4"
-    color="surface-variant"
-    href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-    rel="noopener noreferrer"
+    class="pa-6 bg-white quiz-card"
     rounded="lg"
-    subtitle="Explore available framework Features."
-    target="_blank"
-    title="Features"
-    variant="text"
+    height="96px"
+    flat
   >
-    <v-overlay
-      opacity=".06"
-      scrim="primary"
-      contained
-      model-value
-      persistent
-    />
+    <template #title>
+      <div class="quiz-card__caption">
+        {{ title }}
+      </div>
+    </template>
+    <template #append>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn
+            v-bind="props"
+            flat
+            slim
+            icon="mdi-dots-vertical"
+            size="small"
+          />
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>Редактировать</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-dialog max-width="500">
+              <template v-slot:activator="{ props: activatorProps }">
+                <v-list-item-title v-bind="activatorProps">Удалить</v-list-item-title>
+              </template>
+
+              <template v-slot:default="{ isActive }">
+                <v-card title="Dialog">
+                  <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                      text="Close Dialog"
+                      @click="isActive.value = false"
+                    ></v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
   </v-card>
 </template>
 
 <script setup lang="ts">
-  //
+  defineProps<{
+    title?: string
+  }>()
 </script>
+
+<style lang="scss" scoped>
+.quiz-card {
+  border: 1px solid #E0E0E0 !important;
+
+  &__caption {
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+    letter-spacing: 0.5px;
+    color: #2196F3;
+    white-space: normal;
+  }
+
+  :deep(.v-card-item) {
+    padding: 0;
+  }
+}
+</style>
--- src/pages/quizzes/index.vue
+++ src/pages/quizzes/index.vue
@@ -2,12 +2,18 @@
   <!-- <HelloWorld /> -->
   <h3>Quizzes page</h3>
   <v-row>
-    <v-col cols="6">
-      <quiz-card />
+    <v-col cols="6" v-for="(title, titleIndex) in titles" :key="`title-key-${titleIndex}`">
+      <quiz-card :title="title" />
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts" setup>
-  //
+  const titles = ref([
+    'Опрос пользователей мобильного приложения ...',
+    'Опрос пользователей мобильного приложения ...',
+    'Опрос пользователей мобильного приложения ...',
+    'Опрос пользователей мобильного приложения ...',
+    'Опрос пользователей мобильного приложения ...'
+  ])
 </script>
