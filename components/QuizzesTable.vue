416cb7f44f70e978bce068b0b483fc73ba64c9a0
Vadim Kushir <vadim.kushnir@logity.tech>
2024-03-25 05:16:59 +0500

    feat: implemented the quizzes fetching from the server & changed the project structure

---
+++ components/QuizzesTable.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row>
+    <v-col cols="6" v-for="quiz in quizzes" :key="`title-key-${quiz.id}`">
+      <quiz-card :title="quiz.title" />
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts" setup>
+  const { data: quizzes } = await useFetch('http://127.0.0.1:8000/api/quizzes/', {
+    lazy: true,
+    server: false
+  })
+</script>
