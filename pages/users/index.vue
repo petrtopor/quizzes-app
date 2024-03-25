22aa0e0046988bff119808376b8c63f5100fee46
Vadim Kushir <vadim.kushnir@logity.tech>
2024-03-25 06:05:47 +0500

    feat: implemented users fetching from the server & their displaying in the table

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <!-- <HelloWorld /> -->
   <h3>Users page</h3>
+  <Suspense>
+    <template #default>
+      <users-table />
+    </template>
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
 </template>
 
 <script lang="ts" setup>
-  //
 </script>
