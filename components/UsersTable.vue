adf574a0ce81407c8622a7461f787be4b4a592db
Vadim Kushir <vadim.kushnir@logity.tech>
2024-03-27 01:46:09 +0500

    feat: implemented user editing & deletion

--- components/UsersTable.vue
+++ components/UsersTable.vue
@@ -25,11 +25,69 @@
           <tr
             v-for="item in users"
             :key="item.name"
+            @click="$router.push(`/users/${item.id}`)"
           >
             <td>{{ item.full_name }}</td>
             <td>{{ item.email }}</td>
             <td>{{ item.phone }}</td>
-            <td>{{ item.status }}</td>
+            <td>
+              <v-row align="center">
+                <v-col>
+                  {{ item.status }}
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col>
+                  <v-menu>
+                    <template v-slot:activator="{ props }">
+                      <v-btn
+                        v-bind="props"
+                        flat
+                        slim
+                        icon="mdi-dots-vertical"
+                        size="small"
+                      />
+                    </template>
+                    <v-list>
+                      <v-list-item>
+                        <v-list-item-title @click="$router.push(`/users/${item.id}/edit/`)">
+                          Редактировать
+                        </v-list-item-title>
+                      </v-list-item>
+                      <v-list-item>
+                        <v-dialog max-width="500">
+                          <template v-slot:activator="{ props: activatorProps }">
+                            <v-list-item-title v-bind="activatorProps">Удалить</v-list-item-title>
+                          </template>
+    
+                          <template v-slot:default="{ isActive }">
+                            <v-card title="Подтверждение">
+                              <v-card-text>
+                                Вы хотите удалить пользователя? Отменить действие будет невозможно.
+                              </v-card-text>
+    
+                              <v-card-actions>
+                                <v-spacer></v-spacer>
+
+                                <v-btn
+                                  color="blue"
+                                  text="Удалить"
+                                  @click="onDeleteClick(item.id)"
+                                  :loading="isLoading"
+                                />
+                                <v-btn
+                                  text="Отменить"
+                                  @click="isActive.value = false"
+                                />
+                              </v-card-actions>
+                            </v-card>
+                          </template>
+                        </v-dialog>
+                      </v-list-item>
+                    </v-list>
+                  </v-menu>
+                </v-col>
+              </v-row>
+            </td>
           </tr>
         </tbody>
       </v-table>
@@ -46,8 +104,24 @@
     status: 'ACTIVE';
   }
 
-  const { data: users }: { data: User[] } = await useFetch('http://127.0.0.1:8000/api/users/', {
+  const { data: users, refresh } = await useFetch('http://127.0.0.1:8000/api/users/', {
     lazy: true,
     server: false
   })
+
+  const isLoading = ref(false)
+
+  const onDeleteClick = async (id: string) => {
+    isLoading.value = true
+    try {
+      const response = await $fetch(`http://127.0.0.1:8000/api/users/${id}/`, {
+        method: 'DELETE'
+      })
+    } catch(error) {
+      console.error(error)
+    } finally {
+      isLoading.value = false
+      refresh()
+    }
+  }
 </script>
