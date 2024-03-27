36ad4aee454d8fe1945a103de6e0b4297a0b5c11
Vadim Kushir <vadim.kushnir@logity.tech>
2024-03-27 20:54:31 +0500

    feat: implemented menu actions

--- pages/users/[id]/index.vue
+++ pages/users/[id]/index.vue
@@ -6,49 +6,100 @@
         <v-row>
           <v-col cols="3">
             <!-- I am user with id: {{ $route.params.id }} -->
-            avatar
+            <!-- avatar -->
+            <v-icon
+              icon="mdi-account-circle"
+              size="192"
+            />
           </v-col>
           <v-col cols="6">
-            <v-row>
-              <v-col cols="5">
+            <v-row align="center">
+              <v-col cols="2" class="font-weight-bold">
                 ФИО
               </v-col>
               <v-spacer></v-spacer>
-              <v-col cols="6">
-                Жмышенко Валерий Альбертович
+              <v-col cols="9">
+                {{ full_name }}
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">
+              <v-col cols="2" class="font-weight-bold">
                 Email
               </v-col>
               <v-spacer></v-spacer>
-              <v-col cols="6">
-                Жмышенко Валерий Альбертович
+              <v-col cols="9">
+                {{ email }}
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">
+              <v-col cols="2" class="font-weight-bold">
                 Телефон
               </v-col>
               <v-spacer></v-spacer>
-              <v-col cols="6">
-                +7 666 228 1488
+              <v-col cols="9">
+                {{ phone }}
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">
+              <v-col cols="2" class="font-weight-bold">
                 Статус
               </v-col>
               <v-spacer></v-spacer>
-              <v-col cols="6">
-                BOP B ZAKOHE
+              <v-col cols="9">
+                {{ status }}
               </v-col>
             </v-row>
           </v-col>
           <v-spacer></v-spacer>
           <v-col cols="1">
-            menu
+            <v-menu>
+              <template v-slot:activator="{ props }">
+                <v-btn
+                  v-bind="props"
+                  flat
+                  slim
+                  icon="mdi-dots-vertical"
+                  size="small"
+                />
+              </template>
+              <v-list>
+                <v-list-item>
+                  <v-list-item-title @click="$router.push(`/users/${id}/edit/`)">
+                    Редактировать
+                  </v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                  <v-dialog max-width="500">
+                    <template v-slot:activator="{ props: activatorProps }">
+                      <v-list-item-title v-bind="activatorProps">Удалить</v-list-item-title>
+                    </template>
+
+                    <template v-slot:default="{ isActive }">
+                      <v-card title="Подтверждение">
+                        <v-card-text>
+                          Вы хотите удалить пользователя? Отменить действие будет невозможно.
+                        </v-card-text>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+
+                          <v-btn
+                            color="blue"
+                            text="Удалить"
+                            @click="onDeleteClick()"
+                            :loading="isLoading"
+                          />
+                          <v-btn
+                            text="Отменить"
+                            @click="isActive.value = false"
+                          />
+                        </v-card-actions>
+                      </v-card>
+                    </template>
+                  </v-dialog>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           </v-col>
         </v-row>
       </v-container>
@@ -60,4 +111,24 @@
 </template>
 
 <script lang="ts" setup>
+const { params } = useRoute()
+const { back } = useRouter()
+const { data, refresh } = await useFetch(`http://127.0.0.1:8000/api/users/${params.id}/`)
+const { id, full_name, email, phone, status } = data.value
+
+const isLoading = ref(false)
+
+const onDeleteClick = async () => {
+  isLoading.value = true
+  try {
+    const response = await $fetch(`http://127.0.0.1:8000/api/users/${id}/`, {
+      method: 'DELETE'
+    })
+  } catch(error) {
+    console.error(error)
+  } finally {
+    isLoading.value = false
+    back()
+  }
+}
 </script>
