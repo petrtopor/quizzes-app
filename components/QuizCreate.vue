b09353b8b0105ff8417f909b20ca1e2070f4c4e5
Vadim Kushir <vadim.kushnir@logity.tech>
2024-03-29 12:05:37 +0500

    feat: implemented form submission

--- components/QuizCreate.vue
+++ components/QuizCreate.vue
@@ -250,7 +250,7 @@
             <v-row>
               <v-col>
                 <v-autocomplete
-                  label="Autocomplete"
+                  placeholder="Введите имя участника"
                   :loading="isUsersLoading"
                   :items="users.map(user => ({ title: user.full_name, value: user.id }))"
                   variant="outlined"
@@ -279,8 +279,8 @@
             <v-btn v-if="step < 5" type="submit" color="#2196F3" @click="onClickNext">
               Далее
             </v-btn>
-            <v-btn v-else type="submit" color="#2196F3">
-              Создать
+            <v-btn v-else type="submit" color="#2196F3" @click="submit">
+              {{ !$route.href.includes('create') && !$route.params?.id ? 'Coxpaнить' : 'Создать' }}
             </v-btn>
           </v-col>
         </v-row>
@@ -445,35 +445,7 @@
     try {
       const response = await $fetch(`http://127.0.0.1:8000/api/quizzes${!params?.id ? '' : `/${params?.id}`}/`, {
         method: !params?.id ? 'POST' : 'PUT',
-        body: {
-          "title": "string",
-          "execution_time": 2147483647,
-          "questions": [
-            {
-              "id": 0,
-              "text": "string",
-              "type": "Single",
-              "answers": [
-                {
-                  "text": "string",
-                  "next_question_id": 2147483647
-                }
-              ]
-            }
-          ],
-          "logic": {
-            "type": "Dependent"
-          },
-          "conditions": {
-            "access": "Always",
-            "expiration_date": "2024-03-27"
-          },
-          "assignment": [
-            {
-              "id": 3
-            }
-          ]
-        }
+        body: form
       })
     } catch(error) {
       console.error(error)
